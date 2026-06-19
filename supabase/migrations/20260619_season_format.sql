7db16019cdd7b4919f6b7ef5df317eccc11b9cf2
Schreiter <jonas.doumi@gmail.com>
2026-06-19 14:19:09 +0200

    fix: drop old year check constraint before changing column type

--- supabase/migrations/20260619_season_format.sql
+++ supabase/migrations/20260619_season_format.sql
@@ -1,2 +1,5 @@
 -- Change year column to text for season format (e.g., "18/19", "19/20")
+-- Drop old check constraint first, then change type, then add new constraint
+ALTER TABLE cards DROP CONSTRAINT "cards_year_check";
 ALTER TABLE cards ALTER COLUMN year TYPE text;
+ALTER TABLE cards ADD CONSTRAINT "cards_year_check" CHECK (year ~ '^\d{2}/\d{2}$');
